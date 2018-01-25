f5ad16aa549fd92dbd0e0160106017222af450dd
akintola olalekan <akinlekan@gmail.com>
2018-01-25 19:13:17 +0100

    Added public assets config

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/stripeRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Serve up production assets
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
